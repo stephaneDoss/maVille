cd3a1286a8c4904e546ee70b089a8b1cd828f9e4
Stéphane Dossou <sedegnon.dossou@epitech.eu>
2025-01-22 13:31:24 +0100

    reset

---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    MONGO_URI = "mongodb+srv://sedegnondossou:hWnlTWlWzrHgO00H@cluster0.muylz.mongodb.net/voirie_db?retryWrites=true&w=majority"
+    SECRET_KEY = 'votre_cle_secrète'  # Assurez-vous que cette clé est secrète et difficile à deviner
